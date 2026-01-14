ab2981af45c8ed50201bd64fc145c22b8dc657dd
Uyin Solomon <34033122+rootleet@users.noreply.github.com>
2026-01-14 03:50:57 +0000

    Fix URLs in index.html, enhance form validation in validate.js, and update error messages for better clarity.

--- js/validate.js
+++ js/validate.js
@@ -14,38 +14,51 @@
 
       let thisForm = this;
 
-      let action = thisForm.getAttribute('action');
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-      
-      if( ! action ) {
-        displayError(thisForm, 'The form action property is not set!');
-        return;
-      }
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      thisForm.querySelector('.sent-message').classList.remove('d-block');
+      let name = document.getElementById('name')?.value || '';
+      let email = document.getElementById('email')?.value || '';
+      let Subject = document.getElementById('Subject')?.value || '';
+      let message = document.getElementById('message')?.value || '';
+      let phone = document.getElementById('phone')?.value || '';
 
-      let formData = new FormData( thisForm );
 
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
-              displayError(thisForm, error);
-            }
-          });
-        } else {
-          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+
+      let payload = {
+        module:'load_stream',
+        data:{
+          "source":message,
+          "phone":phone,
+          "email":email,
+          "subject":'website',
+          "message":Subject,
+          name:name
         }
-      } else {
-        php_email_form_submit(thisForm, action, formData);
       }
+
+      console.table(payload)
+      thisForm.querySelector('.loading').classList.add('d-block');
+      fetch('http://127.0.0.1:8000/crm/api/', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Requested-With': 'XMLHttpRequest'
+        },
+        body: JSON.stringify(payload)
+      })
+      .then(response => response.json())
+      .then(data => {
+        if(data['status_code'] === 200){
+          thisForm.querySelector('.loading').classList.remove('d-block');
+          thisForm.querySelector('.error-message').classList.remove('d-block');
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+        } else {
+          displayError(thisForm,data['message'])
+        }
+      })
+      .catch(error => {
+        console.error('Error sending payload:', error);
+      });
+
+      
     });
   });
 
@@ -80,6 +93,12 @@
     thisForm.querySelector('.loading').classList.remove('d-block');
     thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+  function displaySuccess(thisForm,message){
+    thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
   }
 
 })();
